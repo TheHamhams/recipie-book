c36c618be25ddaabe751f59e6bbb94412e130c40
Hamhams <hamhams86@gmail.com>
2021-05-30 08:53:04 -0700

    completed category_menu and edited some formating

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from hash import HashMap
-from recipie import Recipie
+from recipe import Recipe
 
 categories = []
 bad_input = "\nI'm sorry, I didn't recognize that command. Please try again.\n"
@@ -7,15 +7,15 @@ test = HashMap('test', 25)
 categories.append(test)
 
 def program_start():
-    print("Welcome to your recipie book! What would you like to do?")
+    print("Welcome to your recipe book! What would you like to do?")
     response = input("""
     Choose from the following options:
 
     'category': Choose a category to open
     'add': Add a new category
     'delete': delete an existing category
-    'ingredient': Search for recipies that contain a certain ingredient
-    'tag': Search for recipies that contain a certain tag
+    'ingredient': Search for recipes that contain a certain ingredient
+    'tag': Search for recipes that contain a certain tag
     'exit': Exit program
 
     """).lower()
@@ -58,16 +58,29 @@ def print_categories():
 
 def category_menu(category):
     response = input(f"""
-    
     {category.name}
 
-    'add': Add a new recipie
-    'delete': Delete a recipie
-    'select': Select a recipie by name
+    'add': Add a new recipe
+    'delete': Delete a recipe
+    'select': Select a recipe by name
     'main': Go back to main menu
     'exit': Exit program
 
     """).lower()
+
+    if response == 'add':
+        add_recipe_menu(category)
+    elif response == 'delete':
+        delete_recipe_menu(category)
+    elif response == 'select':
+        select_recipe(category)
+    elif response == 'main':
+        program_start()
+    elif response == 'exit':
+        exit_book()
+    else:
+        print(bad_input)
+        category_menu(category)
 
 def add_category_menu():
     pass
@@ -82,8 +95,17 @@ def ingredient_search_menu():
 def tag_search_menu():
     pass
 
+def add_recipe_menu(category):
+    pass
+
+def delete_recipe_menu(category):
+    pass
+
+def select_recipe(category):
+    pass
+
 def exit_book():
-    print("Happy cooking!")
+    print("\nHappy cooking!")
     exit()
 
 
