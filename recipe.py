6cf57bcdbe9f006afd276d4b379e4ae934166c58
Hamhams <hamhams86@gmail.com>
2021-05-30 15:35:48 -0700

    created recipe method placeholders

--- recipe.py
+++ recipe.py
@@ -4,3 +4,40 @@ class Recipe:
         self.ingredients = []
         self.steps = []
         self.tags = []
+        
+
+    def add_ingredients(self, ingredient):
+        pass
+
+    def edit_ingredients(self):
+        pass
+
+    def add_steps(self):
+        pass
+
+    def print_recipe(self):
+        pass
+
+    def print_ingredients(self):
+        pass
+
+    def print_steps(self):
+        pass
+
+    def print_tags(self):
+        pass
+
+    def edit_category(self, category):
+        pass
+
+    def print_category(self):
+        pass
+
+    def add_tag(self, tag):
+        self.tags.append(tag)
+
+    def edit_tag(self):
+        pass
+
+    def print_tags(self):
+        print(self.tags)
